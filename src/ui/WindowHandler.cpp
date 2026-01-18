7cb0ed27e9bd0924c47753dadaa8c387b4a510a6
YarikMamykin <ymamykin@gmail.com>
2026-01-18 16:53:30 +0200

    Lower FPS

--- src/ui/WindowHandler.cpp
+++ src/ui/WindowHandler.cpp
@@ -5,7 +5,7 @@
 namespace ymwm::ui {
   WindowHandler::WindowHandler() {
     InitWindow(100, 100, "YMWM-SB");
-    SetTargetFPS(60);
+    SetTargetFPS(20);
   }
 
   WindowHandler::~WindowHandler() { CloseWindow(); }
