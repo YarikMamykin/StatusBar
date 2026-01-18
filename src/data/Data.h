8dd4a1c5a7cf1f39d8c785dd603d52e846633200
YarikMamykin <ymamykin@gmail.com>
2026-01-18 02:42:05 +0200

    Define data parts internals

--- src/data/Data.h
+++ src/data/Data.h
@@ -1,14 +1,42 @@
 #pragma once
+#include <chrono>
 #include <variant>
+#include <vector>
 
 namespace ymwm::data {
 
-  struct Date {};
-  struct Time {};
-  struct Cpu {};
-  struct Ram {};
-  struct Drive {};
-  struct Battery {};
+  struct Date {
+    std::chrono::system_clock::time_point data;
+  };
+
+  struct Time {
+    std::chrono::system_clock::time_point data;
+  };
+
+  struct Cpu {
+    std::vector<unsigned char> cores_load_lvl;
+    unsigned char load_lvl;
+  };
+
+  struct Ram {
+    unsigned int megabytes;
+    unsigned char level;
+  };
+
+  struct Drive {
+    std::size_t bytes;
+    unsigned char level;
+  };
+
+  struct Battery {
+    unsigned char level;
+    enum class BatteryStatus : unsigned char {
+      NO_BATTERY,
+      DISCHARGING,
+      CHARGING,
+      FULL
+    } status;
+  };
 
   using Data =
       std::variant<std::monostate, Date, Time, Cpu, Ram, Drive, Battery>;
